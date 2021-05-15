cdb8b020eb42d56ebda4a32d77378c607d1125f6
Arkadiusz Pabisz <arkadiusz.pabisz@gmail.com>
2021-05-15 18:17:01 +0200

    feat: Buy button for courses

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,13 +1,37 @@
-import React from "react"
+import React, { useContext } from "react"
+import { useHistory } from "react-router"
 import bemCssModules from "bem-css-modules"
+
+import { StoreContext } from "../../stores/StoreProvider"
+import request from "../../helpers/request"
 
 import { default as CourseStyles } from "./Course.module.scss"
 
 const block = bemCssModules(CourseStyles)
 
-const Course = ({ authors, img, price, title }) => {
-  const allAuthors = authors.join(", ")
+const Course = ({ authors, id, img, isUserContext = false, price, title }) => {
+  const { user, setUser } = useContext(StoreContext)
+  const history = useHistory()
 
+  const allAuthors = authors.join(", ")
+  const isUserLogged = Boolean(user)
+
+  const handleOnClick = async () => {
+    try {
+      const { data, status } = await request.patch("/users", {
+        login: user.login,
+        courseId: id,
+      })
+      if (status === 202) {
+        setUser(data.user)
+        history.push("/my-courses")
+      }
+    } catch (error) {
+      console.warn(erorr)
+    }
+  }
+
+  const shouldBeBuyButtonVisible = isUserLogged && !isUserContext
   return (
     <li>
       <article className={block()}>
@@ -15,6 +39,9 @@ const Course = ({ authors, img, price, title }) => {
         <img src={img} alt='title' className={block("image")} />
         <p className={block("price")}>{`Koszt kursu: ${price}`}</p>
         <p className={block("authors")}>{`Autorzy kursu: ${allAuthors}`}</p>
+        {shouldBeBuyButtonVisible && (
+          <button onClick={handleOnClick}>Kup ten kurs</button>
+        )}
       </article>
     </li>
   )
