a6932337728c9b28305d0c1b3f2c2ffebaf07cc6
Arek <arkadiusz.pabisz@gmail.com>
2021-06-19 19:37:22 +0200

    change: request for heroku server

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const request = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "https://nodejs-e-learning.herokuapp.com/",
   validateStatus: false,
 })
 
