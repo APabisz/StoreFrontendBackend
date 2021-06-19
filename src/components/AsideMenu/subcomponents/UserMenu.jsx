a7350bb4aac199f62724387f17c545a7a573759a
Arek <arkadiusz.pabisz@gmail.com>
2021-06-19 19:42:34 +0200

    feat: show available budget

--- src/components/AsideMenu/subcomponents/UserMenu.jsx
+++ src/components/AsideMenu/subcomponents/UserMenu.jsx
@@ -1,23 +1,32 @@
-import React from "react"
+import React, { useContext } from "react"
 import bemCssModules from "bem-css-modules"
 import { Link } from "react-router-dom"
+import { StoreContext } from "../../../stores/StoreProvider"
 
 import { default as AsideMenuStyles } from "../AsideMenu.module.scss"
 
 const block = bemCssModules(AsideMenuStyles)
 
 const UserMenu = ({ isUserLogged }) => {
+  const { user } = useContext(StoreContext)
+
   return (
     <>
-      <p className={block("title")}>Panel Użytkownika</p>
+      <p className={block("title")}>User panel</p>
+      {isUserLogged && (
+        <p className={block("money")}>
+          Available budget: <span>{user.budget.toFixed(2)} PLN</span>
+        </p>
+      )}
+
       <nav>
         <ul className={block("list")}>
           <li className={block("link")}>
-            <Link to='/'>Kursy w sprzedaży</Link>
+            <Link to='/'>Curses available to buy</Link>
           </li>
           {isUserLogged && (
             <li className={block("link")}>
-              <Link to='/my-courses'>Moje zakupoione kursy</Link>
+              <Link to='/my-courses'>My courses</Link>
             </li>
           )}
         </ul>
